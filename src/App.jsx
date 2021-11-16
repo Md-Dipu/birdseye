6f8eca7a4f9da47cbadb130b0981f3e05fe18f6a
Md. Dipu <srabun.tangail@gmail.com>
2021-11-16 08:52:55 +0600

    Basic Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
     return (
-        <div>
-            <h3>This is from App.js</h3>
-        </div>
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
     );
 }
 
