2e955eaa80f0e53d54876437c875cc774dab56fb
Md. Dipu <srabun.tangail@gmail.com>
2021-11-16 09:17:11 +0600

    Title changes

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -9,12 +9,12 @@ const Header = () => {
         <>
             <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
             <Container>
-                <Navbar.Brand as={Link} to="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to="#home">Birdeye</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
                     <Nav.Link as={Link} to="#home">Home</Nav.Link>
-                    <Nav.Link as={Link} to="#link">Link</Nav.Link>
+                    <Nav.Link as={Link} to="#link">About us</Nav.Link>
                     <NavDropdown title={<FontAwesomeIcon icon={faUser} />} id="basic-nav-dropdown">
                         <NavDropdown.Item as={Link} to="#action/3.1">My Orders</NavDropdown.Item>
                         <NavDropdown.Item as={Link} to="#action/3.2">Manage All Orders</NavDropdown.Item>
