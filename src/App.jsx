a715b15613c62f02da0d58090f62cd80053b4ad9
Md. Dipu <srabun.tangail@gmail.com>
2021-11-17 15:53:43 +0600

    Plans and Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
+import Plans from './Pages/Plans/Plans.jsx';
+import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -10,8 +12,10 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/plans" element={<Plans />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
+            <Footer />
         </>
     );
 }
