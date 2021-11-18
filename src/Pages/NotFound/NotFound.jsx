e6932caa6c73173e74ec8b9957d6ecac545b80f7
Md. Dipu <srabun.tangail@gmail.com>
2021-11-18 15:22:57 +0600

    Modified NotFound Page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Button, ButtonGroup, Image } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
-            <h1>404</h1>
-            <p>page not fond</p>
+            <Image src="https://i.ibb.co/BfFjb2r/404.jpg" className="d-block mx-auto" fluid />
+            <div className="text-center mb-3">
+                <h6>404: Page not found</h6>
+                <ButtonGroup>
+                    <Button variant="secondary" onClick={() => navigate('/#home')}>Go Home</Button>
+                    <Button variant="warning" onClick={() => navigate(-1)}>Go Back</Button>
+                </ButtonGroup>
+            </div>
         </div>
     );
 };
