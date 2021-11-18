6a871a0715ead0ceb9ecb964ab344df313031b4e
Md. Dipu <srabun.tangail@gmail.com>
2021-11-18 20:55:22 +0600

    remote server replaced and paginetion

--- src/Pages/Plans/Plans.jsx
+++ src/Pages/Plans/Plans.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, ButtonGroup, Col, Container, Row } from 'react-bootstrap';
 import Plan from '../Plan/Plan';
 import Loading from '../Shared/Loading/Loading';
 
@@ -8,16 +8,17 @@ const Plans = () => {
     const [totalPlans, setTotalPlans] = useState(0);
     const [plans, setPlans] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    const [currentPage, setCurrentPage] = useState(0);
+    const limit = 1;
     useEffect(() => {
-        axios.get('http://localhost:5000/plans')
+        axios.get(`https://calm-tor-26955.herokuapp.com/plans?limit=${limit}&&page=${currentPage}`)
             .then(res => {
                 setTotalPlans(res.data.count);
                 setPlans(res.data.plans);
             })
             .catch(error => console.warn(error))
             .then(() => setLoading(false));
-    }, []);
+    }, [currentPage]);
 
     if (loading) {
         return <Loading height="80" />;
@@ -25,7 +26,7 @@ const Plans = () => {
 
     return (
         <Container>
-            <h3 className="text-center text-uppercase my-3">our all plans {totalPlans}</h3>
+            <h3 className="text-center text-uppercase my-3">our all plans</h3>
             <Row xs={1} md={2} lg={3} className="g-4 mb-4">
                 {plans.map(plan => (
                     <Col key={plan.key}>
@@ -33,6 +34,17 @@ const Plans = () => {
                     </Col>
                 ))}
             </Row>
+            {limit < totalPlans && <div className="text-center my-3">
+                <ButtonGroup className="text-center">
+                    {[...Array(Math.ceil(totalPlans/limit)).keys()]
+                    .map(page => (
+                        <Button 
+                            variant={page === currentPage ? 'primary' : 'outline-primary'}
+                            onClick={() => setCurrentPage(page)}
+                        >{page+1}</Button>
+                    ))}
+                </ButtonGroup>
+            </div>}
         </Container>
     );
 };
