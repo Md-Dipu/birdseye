8f9c7a780c591d8700459fd5be0158bb3ad47bac
Md. Dipu <srabun.tangail@gmail.com>
2021-11-18 15:23:21 +0600

    Show of plans for home page

---
+++ src/Pages/Home/ShowPlans/ShowPlans.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row, Spinner } from 'react-bootstrap';
+import Plan from '../../Plan/Plan';
+
+const ShowPlans = () => {
+    const [plans, setPlans] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/plans')
+            .then(res => {
+                setPlans(res.data.plans);
+            })
+            .catch(error => console.warn(error))
+            .then(() => setLoading(false));
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '50vh' }}>
+                <Spinner animation="border"  variant="success" />
+            </div>
+        );
+    }
+
+    return (
+        <Container>
+            <h3 className="text-center text-uppercase mt-3">our plans</h3>
+            <Row xs={1} md={2} lg={3} className="g-4 my-3">
+                {plans.map(plan => (
+                    <Col key={plan.key}>
+                        <Plan plan={plan} />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default ShowPlans;
