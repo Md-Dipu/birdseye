80205b4215fee03ecb711cddec19f7c6a9804f29
Md. Dipu <srabun.tangail@gmail.com>
2021-11-18 15:23:54 +0600

    How page with Banner and ShowPlans

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import ShowPlans from '../ShowPlans/ShowPlans';
+
+const Home = () => {
+    return (
+        <div id="home">
+            <Banner />
+            <ShowPlans />
+        </div>
+    );
+};
+
+export default Home;
