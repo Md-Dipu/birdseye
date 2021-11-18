56f80af38452aad1c5514093934c6263c37faed8
Md. Dipu <srabun.tangail@gmail.com>
2021-11-18 15:24:13 +0600

    Simple Banner

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="text-center text-white tourism-banner">
+            <h1 className="banner-title text-uppercase">Travel and Learn</h1>
+            <h3>Trave is the way to learn more and stay peaceful.</h3>
+        </div>
+    );
+};
+
+export default Banner;
