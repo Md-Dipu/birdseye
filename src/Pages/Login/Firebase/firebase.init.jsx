18a901246909a4061225369f1a7d47a3eb33c201
Md. Dipu <srabun.tangail@gmail.com>
2021-11-18 20:57:03 +0600

    Firebase Initalizer function

---
+++ src/Pages/Login/Firebase/firebase.init.jsx
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+const firebaseAppInitializer = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default firebaseAppInitializer;
