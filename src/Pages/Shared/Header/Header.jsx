4a5d02e9330f7d985eddd2b71ded960e32811ff5
Md. Dipu <srabun.tangail@gmail.com>
2021-11-19 10:16:41 +0600

    Logout method

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faSignOutAlt, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import useAuth from '../../../hooks/useAuth';
 
 const Header = () => {
-    const [user, setUser] = useState(null);
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
     return (
         <>
             <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
@@ -17,13 +19,13 @@ const Header = () => {
                     <Nav.Link as={Link} to="/#home">Home</Nav.Link>
                     <Nav.Link as={Link} to="/plans">All Plans</Nav.Link>
                     <Nav.Link as={Link} to="/about">About us</Nav.Link>
-                    {!user ?  <Nav.Link as={Button} variant="primary" onClick={() => setUser(true)}><FontAwesomeIcon icon={faSignInAlt} /> Login</Nav.Link> :
-                    <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> Md. Dipu</>} menuVariant="dark" id="basic-nav-dropdown">
+                    {!user ?  <Nav.Link as={Button} variant="primary" onClick={() => navigate('/login')}><FontAwesomeIcon icon={faSignInAlt} /> Login</Nav.Link> :
+                    <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> {user.displayName}</>} menuVariant="dark" id="basic-nav-dropdown">
                         <NavDropdown.Item as={Link} to="/my-orders">My Orders</NavDropdown.Item>
                         <NavDropdown.Item as={Link} to="/manage-all-orders">Manage All Orders</NavDropdown.Item>
                         <NavDropdown.Item as={Link} to="/add-new-plan">Add a New Plan</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item as={Button} variant="link" onClick={() => setUser(null)}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
+                        <NavDropdown.Item as={Button} variant="link" onClick={logOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
                     </NavDropdown>}
                 </Nav>
                 </Navbar.Collapse>
