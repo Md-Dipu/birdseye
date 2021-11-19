a15413ff7a10e3fe925aec72f3accb5bb0ab65b0
Md. Dipu <srabun.tangail@gmail.com>
2021-11-19 10:37:18 +0600

    Applied route protector on plans and login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/Home/Home';
-import NotFound from './Pages/NotFound/NotFound';
-import Plans from './Pages/Plans/Plans.jsx';
-import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Plans from './Pages/Plans/Plans.jsx';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import AuthProvider from './context/AuthProvider';
+import RouteProtector from './Pages/Login/PrivateRoute/RouteProtector';
 
 function App() {
     return (
-        <>
+        <AuthProvider>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/plans" element={<Plans />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/plans" element={<RouteProtector element={<Plans />} from='/plans' />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
-        </>
+        </AuthProvider>
     );
 }
 
