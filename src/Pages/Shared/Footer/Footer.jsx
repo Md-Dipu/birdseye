9e23d469282631eca9984eff3906cc4e18c79de2
Md. Dipu <srabun.tangail@gmail.com>
2021-11-25 15:30:17 +0600

    Used react-hook-form and reset action

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const Footer = () => {
-    const handleSubscribe = e => {
-        e.preventDefault();
+    const { register, handleSubmit, reset } = useForm();
+    const handleSubscribe = data => {
+        console.log(data.email);
+        reset();
     }
+    
     return (
         <div className="bg-light py-4">
             <Container>
@@ -26,8 +30,8 @@ const Footer = () => {
                             <Col>
                                 <h4>Stay Updated</h4>
                                 <p>To get message from your site please subscribe us by email address.</p>
-                                <form onSubmit={handleSubscribe}>
-                                    <input type="email" className="form-control" placeholder="Email" />
+                                <form onSubmit={handleSubmit(handleSubscribe)}>
+                                    <input type="email" className="form-control" placeholder="Email" {...register('email')} />
                                     <input type="submit" className="btn btn-primary form-control mt-2" value="Subscribe" />
                                 </form>
                             </Col>
