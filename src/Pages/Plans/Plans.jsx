f3851535c5a66d20af0686606366383bc30698e3
Md. Dipu <srabun.tangail@gmail.com>
2021-11-26 19:26:41 +0600

    Change pagination system

--- src/Pages/Plans/Plans.jsx
+++ src/Pages/Plans/Plans.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Button, ButtonGroup, Col, Container, Row } from 'react-bootstrap';
+import { useHistory, useLocation } from 'react-router';
+import { backToTop } from '../../utilities/utilities';
 import Plan from '../Plan/Plan';
 import Loading from '../Shared/Loading/Loading';
 
@@ -8,14 +10,26 @@ const Plans = () => {
     const [totalPlans, setTotalPlans] = useState(0);
     const [plans, setPlans] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(0);
+
+    // pagination
+    const history = useHistory();
+    const location = useLocation();
+    const searchQueary = new URLSearchParams(location.search);
+    const currentPage = (parseInt(searchQueary.get('page')) - 1) || 0;
+    const setCurrentPage = pageNumber => {
+        searchQueary.set('page', String(pageNumber + 1));
+        location.search = searchQueary.toString();
+        history.push(location);
+    };
     
-    const limit = 9;
+    // plan per page
+    const limit = 3;
     useEffect(() => {
         axios.get(`http://localhost:5000/plans?limit=${limit}&&page=${currentPage}`)
             .then(res => {
                 setTotalPlans(res.data.count);
                 setPlans(res.data.plans);
+                backToTop();
             })
             .catch(error => console.warn(error))
             .then(() => setLoading(false));
