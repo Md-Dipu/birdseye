a69a248cebf316659148b98c64c3c407b28e40cf
Md. Dipu <srabun.tangail@gmail.com>
2021-12-01 13:07:20 +0600

    basic booked action

--- src/utilities/API.jsx
+++ src/utilities/API.jsx
@@ -6,3 +6,14 @@ export const addPlanDB = newPlan => {
         .then(res => alert(`Adding Status:${res.statusText} with plan id:${res.data.insertedId}`))
         .catch(error => console.warn(error));
 }
+
+
+// user API
+export const updateUserBookedDB = (user, planTicket) => {
+    axios.put('http://localhost:5000/users', {
+        user,
+        planTicket
+    })
+        .then(res => console.log(res.data))
+        .catch(error => console.warn(error));
+}
