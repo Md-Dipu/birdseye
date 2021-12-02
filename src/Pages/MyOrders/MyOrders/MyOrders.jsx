ad55208f223c65f4a0440af20014b443a5829076
Md. Dipu <srabun.tangail@gmail.com>
2021-12-02 16:24:31 +0600

    Observe cancel and loading system

--- src/Pages/MyOrders/MyOrders/MyOrders.jsx
+++ src/Pages/MyOrders/MyOrders/MyOrders.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import useAuth from '../../../hooks/useAuth';
+import Loading from '../../Shared/Loading/Loading';
 import MyOrder from '../MyOrder/MyOrder';
 
 const MyOrders = () => {
     const [orderedList, setOrderedList] = useState({});
-    const [isCanceled, setIsCanceled] = useState(false);
+    const [observeCancel, setObserveCancel] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const { user } = useAuth();
 
     const orderedItems = Object.keys(orderedList);
@@ -14,22 +16,30 @@ const MyOrders = () => {
     useEffect(() => {
         axios.get(`http://localhost:5000/users/${user.email}`)
             .then(res => setOrderedList(res?.data?.ordered))
-            .catch(error => console.warn(error));
-    }, [user, isCanceled]);
+            .catch(error => console.warn(error))
+            .then(() => setIsLoading(false));
+    }, [user, observeCancel]);
+
+    if (isLoading) {
+        return <Loading height="80" />;
+    }
 
     return (
-        <Row className="justify-content-center my-3">
-            <Col xs={12} md={8} lg={6}>
-                <h4 className="text-uppercase text-center">your <span className="text-info">orders</span></h4>
-                {orderedItems.map((planId, _idx) => <MyOrder 
-                    key={_idx} 
-                    user={user}
-                    planId={planId} 
-                    orderedList={orderedList}
-                    setIsCanceled={setIsCanceled}
-                />)}
-            </Col>
-        </Row>
+        <Container>
+            <Row className="justify-content-center my-3">
+                <Col xs={12} md={8} lg={6}>
+                    <h4 className="text-uppercase text-center">your <span className="text-info">orders</span></h4>
+                    {orderedItems.map((planId, _idx) => <MyOrder 
+                        key={_idx} 
+                        user={user}
+                        planId={planId} 
+                        orderedList={orderedList}
+                        observeCancel={observeCancel}
+                        setObserveCancel={setObserveCancel}
+                    />)}
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
