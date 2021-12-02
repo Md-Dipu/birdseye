52b57776a3a1499c1ad6b476d7b84cbc67f70ba6
Md. Dipu <srabun.tangail@gmail.com>
2021-12-02 14:24:50 +0600

    Basic MyOrders

---
+++ src/Pages/MyOrders/MyOrders/MyOrders.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import useAuth from '../../../hooks/useAuth';
+import MyOrder from '../MyOrder/MyOrder';
+
+const MyOrders = () => {
+    const [orderedList, setOrderedList] = useState({});
+    const { user } = useAuth();
+
+    const orderedItems = Object.keys(orderedList);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/users/${user.email}`)
+            .then(res => setOrderedList(res?.data?.ordered))
+            .catch(error => console.warn(error));
+    }, [user]);
+
+    return (
+        <Container className="my-3">
+            <h4 className="text-uppercase text-center">your orders</h4>
+            {orderedItems.map((planId, _idx) => <MyOrder 
+                key={_idx} 
+                planId={planId} 
+                orderDetails={orderedList[planId]} 
+            />)}
+        </Container>
+    );
+};
+
+export default MyOrders;
