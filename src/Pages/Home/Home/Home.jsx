fdcef264fb176f17f8edb267c912b3b1c5f97036
Md. Dipu <srabun.tangail@gmail.com>
2021-12-08 15:13:06 +0600

    Added extra section Feature and HappyClient

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useLocation } from 'react-router';
 import { backToTop } from '../../../utilities/utilities';
 import Banner from '../Banner/Banner';
+import Feature from '../Feature/Feature';
+import HappyClient from '../HappyClient/HappyClient';
 import ShowPlans from '../ShowPlans/ShowPlans';
 
 const Home = () => {
@@ -11,10 +13,12 @@ const Home = () => {
     }
     
     return (
-        <div id="home">
+        <>
             <Banner />
+            <Feature />
             <ShowPlans />
-        </div>
+            <HappyClient />
+        </>
     );
 };
 
