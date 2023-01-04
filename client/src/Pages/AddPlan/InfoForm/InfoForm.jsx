bbc43554dbbb4d02e2d0c76ec636d2f0b858fc1f
Md. Dipu <srabun.tangail@gmail.com>
2023-01-04 15:16:33 +0600

    [modify] first level form modification

--- client/src/Pages/AddPlan/InfoForm/InfoForm.jsx
+++ client/src/Pages/AddPlan/InfoForm/InfoForm.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
 const InfoForm = props => {
-    const { register, handleSubmit, handleAddPlan } = props;
+    const { register, handleSubmit, onSubmit } = props;
     return (
-        <form onSubmit={handleSubmit(handleAddPlan)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <h3 className="text-center my-3 text-uppercase">Add a <span className="text-info">new plan</span></h3>
-            <input type="text" className="form-control mb-3" placeholder="Title" {...register('title', { required: true })} />
-            <input type="text" className="form-control mb-3" placeholder="IBB Image URL" {...register('img_url', { required: true })} />
-            
+            <input type="text" className="form-control mb-3" placeholder="Name" {...register('name', { required: true })} />
+            <input type="file" className="form-control mb-3" placeholder="Cover Image" {...register('image', { required: true })} />
+
             <div className="input-group mb-3">
                 <input type="text" className="form-control" placeholder="Tour Days" {...register('tourDays', { required: true })} />
-                <input type="text" className="form-control" placeholder="Rating" {...register('rating', { required: true })} />
-                <input type="text" className="form-control" placeholder="Cost" {...register('cost', { required: true })} />
+                <input type="text" className="form-control" placeholder="Price" {...register('price', { required: true })} />
             </div>
-            
-            <input type="date" className="form-control mb-3" placeholder="Starting Date" {...register('starting_date', { required: true })} />
-            <textarea className="form-control mb-3" placeholder="Description" rows="6" {...register('description', { required: true })} />
-            
+
+            <input type="date" className="form-control mb-3" placeholder="Starting Date" {...register('startingDate', { required: true })} />
+            <textarea className="form-control mb-3" placeholder="Short Description" rows="6" {...register('shortDescription', { required: true })} />
+
             <div>
                 <input type="submit" value="Done" className="btn btn-primary" />{" "}
                 <input type="reset" value="Cancel" className="btn btn-outline-secondary" />
