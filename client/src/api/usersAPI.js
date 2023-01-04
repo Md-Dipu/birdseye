bf5580bea49ff7b30fcac09b66b6db11d7d5772a
Md. Dipu <srabun.tangail@gmail.com>
2023-01-05 00:23:52 +0600

    [add api frontend] get user by email

--- client/src/api/usersAPI.js
+++ client/src/api/usersAPI.js
@@ -3,7 +3,11 @@ import API from "../utilities/API";
 const userAPI = new API('/api/v1/users');
 
 export const createUser = async (data) => {
-    console.log(data);
     const user = await userAPI.post(data);
+    return user;
+};
+
+export const getUserByEmail = async (email) => {
+    const user = await userAPI.get(`/${email}`);
     return user;
 };
