8a1d13dfc39925a7f78769f01e87f3bc0009e15d
Md. Dipu <srabun.tangail@gmail.com>
2023-01-07 14:37:07 +0600

    [add api] route not found

--- server/index.js
+++ server/index.js
@@ -25,6 +25,13 @@ app.get("/", (req, res) => {
     });
 });
 
+app.all("*", (req, res) => {
+    res.status(404).json({
+        status: "fail",
+        message: "Route not found"
+    });
+});
+
 dbConnection.connectToServer((err) => {
     if (err) {
         console.error(err);
