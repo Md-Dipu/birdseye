8ef11712e3672c90152826ebcf8b681de38e7498
Md. Dipu <srabun.tangail@gmail.com>
2023-01-08 20:29:29 +0600

    [add] auth layout

--- client/src/Pages/Authentication/Login/Login.jsx
+++ client/src/Pages/Authentication/Login/Login.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
-import { Container } from 'react-bootstrap';
 import { useHistory, useLocation } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
 import { backToTop } from '../../../utilities/utilities';
-import QuickAlert from '../../Shared/QuickAlert/QuickAlert';
-import GoogleSignIn from '../GoogleSignIn/GoogleSignIn';
 import LoginForm from '../LoginForm/LoginForm';
+import AuthLayout from '../AuthLayout/AuthLayout';
 
 const Login = () => {
     const [error, setError] = useState(null);
@@ -21,30 +17,17 @@ const Login = () => {
         backToTop();
     }
 
+    const onSuccess = () => history.push(redirectUrl);
     const onError = (error) => {
         setError(null);
         setError(error);
     };
 
+
     return (
-        <Container className="py-4">
-            {user && <QuickAlert variant="warning" heading="You're logged in already!" icon={<FontAwesomeIcon icon={faExclamationTriangle} />}>
-                Please logout to login from another account.
-            </QuickAlert>}
-            {error && <QuickAlert variant="danger" heading={error.heading} icon={<FontAwesomeIcon icon={faExclamationCircle} />}>
-                {error.message}
-            </QuickAlert>}
-            <div className="border p-4 mx-auto my-5 shadow-sm" style={{ maxWidth: 400 }}>
-                <LoginForm onError={onError} />
-                <div className="mt-4">
-                    <GoogleSignIn
-                        className="w-100"
-                        onSuccess={() => history.push(redirectUrl)}
-                        onError={onError}
-                    />
-                </div>
-            </div>
-        </Container>
+        <AuthLayout user={user} error={error} onError={onError} onSuccess={onSuccess}>
+            <LoginForm onError={onError} onSuccess={onSuccess} />
+        </AuthLayout>
     );
 };
 
