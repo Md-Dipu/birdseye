db49bb1ea7b2f5c38674db1ccb671f97a5dbd040
Md. Dipu <srabun.tangail@gmail.com>
2023-01-08 20:06:39 +0600

    [modify] nav links

--- client/src/Pages/Shared/Header/Header.jsx
+++ client/src/Pages/Shared/Header/Header.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faSignOutAlt, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import useAuth from '../../../hooks/useAuth';
 
 const Header = () => {
     const { user, logOut } = useAuth();
-    const history = useHistory();
     return (
-        <>
-            <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
-                <Container>
-                    <Navbar.Brand as={Link} to="/">Birdseye</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/plans">All Plans</Nav.Link>
-                            <Nav.Link as={Link} to="/about-us">About us</Nav.Link>
-                            {!user ? <Nav.Link as={Button} variant="primary" className="text-white" onClick={() => history.push('/login')}><FontAwesomeIcon icon={faSignInAlt} /> Login</Nav.Link> :
-                                <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> {user.name}</>} menuVariant="dark" id="basic-nav-dropdown">
-                                    <NavDropdown.Item as={Link} to="/my-orders">My Orders</NavDropdown.Item>
-                                    <NavDropdown.Item as={Link} to="/manage-all-orders">Manage All Orders</NavDropdown.Item>
-                                    <NavDropdown.Item as={Link} to="/add-new-plan">Add a New Plan</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item as={Button} variant="link" onClick={logOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
-                                </NavDropdown>}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar variant="dark" bg="dark" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Birdseye</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/plans">Plans</Nav.Link>
+                        <Nav.Link as={Link} to="/about-us">About us</Nav.Link>
+                        {!user ? <>
+                            <Nav.Link as={Link} to="/login" variant="link"><FontAwesomeIcon icon={faSignInAlt} /> Login</Nav.Link>
+                            <Nav.Link className="p-0">
+                                <Button as={Link} variant="secondary" to="/register" className="rounded-pill">Register</Button>
+                            </Nav.Link>
+                        </> : <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> {user.name}</>} menuVariant="dark" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/my-orders">My Orders</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/manage-all-orders">Manage All Orders</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/add-new-plan">Add a New Plan</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Button} variant="link" onClick={logOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
+                        </NavDropdown>}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
