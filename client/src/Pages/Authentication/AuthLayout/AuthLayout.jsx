67a5d1eb150cf23bd8fba6e86ccbeb0f2309c062
Md. Dipu <srabun.tangail@gmail.com>
2023-01-08 20:27:30 +0600

    [add] lay out for authentication pages

---
+++ client/src/Pages/Authentication/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import { Container } from 'react-bootstrap';
+import QuickAlert from '../../Shared/QuickAlert/QuickAlert';
+import GoogleSignIn from '../GoogleSignIn/GoogleSignIn';
+
+const AuthLayout = ({ children, user, error, onError, onSuccess }) => {
+    return (
+        <Container className="py-4">
+            {user && <QuickAlert variant="warning" heading="You're logged in already!" icon={<FontAwesomeIcon icon={faExclamationTriangle} />}>
+                Please logout to login from another account.
+            </QuickAlert>}
+            {error && <QuickAlert variant="danger" heading={error.heading} icon={<FontAwesomeIcon icon={faExclamationCircle} />}>
+                {error.message}
+            </QuickAlert>}
+            <div className="border p-4 mx-auto my-5 shadow-sm" style={{ maxWidth: 400 }}>
+                {children}
+                <div className="mt-4">
+                    <GoogleSignIn
+                        className="w-100"
+                        onSuccess={onSuccess}
+                        onError={onError}
+                    />
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default AuthLayout;
