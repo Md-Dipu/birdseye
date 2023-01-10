d3c5b6dec5372878f953880430d325a197c48462
Md. Dipu <srabun.tangail@gmail.com>
2023-01-10 13:24:17 +0600

    [add] search text passing to plans

--- client/src/Pages/Home/Banner/Banner.jsx
+++ client/src/Pages/Home/Banner/Banner.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 import './Banner.css';
 
 const Banner = () => {
+    const inputRef = useRef();
+    const history = useHistory();
+
+    const onSubmit = e => {
+        e.preventDefault();
+        history.push(`/plans?search=${inputRef.current.value}`);
+    };
+
     return (
         <div className="text-center text-light tourism-banner">
             <Container>
                 <Row className="justify-content-center">
                     <Col xs={12} md={8} lg={6}>
-                        <Form className="mb-3" onSubmit={e => e.preventDefault()}>
+                        <Form className="mb-3" onSubmit={onSubmit}>
                             <InputGroup>
-                                <Form.Control type="text" className="home-search-bar" />
+                                <Form.Control
+                                    type="text"
+                                    ref={inputRef}
+                                    className="home-search-bar"
+                                    required
+                                />
                                 <Button
                                     type="submit"
                                     variant="success"
