00651b1408a1874dcdcc86b7722c2d3a179ccc90
Md. Dipu <srabun.tangail@gmail.com>
2023-01-14 23:22:17 +0600

    [add] basic booking card

---
+++ client/src/Pages/Dashboard/Bookings/Booking.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Booking = (props) => {
+    return (
+        <Card className="mb-3 overflow-hidden" style={{ width: '49%' }}>
+            <Card.Body className="p-2">
+                <div className="d-flex">
+                    <img
+                        src={props.coverImageURL}
+                        width="150"
+                        className="rounded"
+                        alt=""
+                    />
+                    <div className="ms-3">
+                        <Card.Subtitle as={Link} to={`/plans/${props.planId}`} className="text-decoration-none text-secondary fw-bold">{props.planName}</Card.Subtitle>
+                        <Card.Subtitle className="text-secondary">Cost: ${props.payableAmount.toFixed(2)}</Card.Subtitle>
+                        <Card.Subtitle className="text-secondary">Quantity: {props.quantity}</Card.Subtitle>
+                        <Button variant="link" size="sm" className="px-0">View details</Button>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Booking;
