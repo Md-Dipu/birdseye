18b9f45da7318d1dc946d37c4d42677e593c5ad0
Md. Dipu <srabun.tangail@gmail.com>
2023-01-14 23:20:36 +0600

    [add] user menu

---
+++ client/src/Pages/Authentication/AuthNavMenu/AuthNavMenu.jsx
@@ -0,0 +1,20 @@
+import { faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Button, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
+
+const AuthNavMenu = () => {
+    const { user, logOut } = useAuth();
+
+    return (
+        <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> {user.name}</>} menuVariant="dark" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to="/dashboard/account">Account</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item as={Button} variant="link" onClick={logOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
+        </NavDropdown>
+    );
+};
+
+export default AuthNavMenu;
