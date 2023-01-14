f1cd49b0ff8b42a8141b7aaf276cf0a4435aa835
Md. Dipu <srabun.tangail@gmail.com>
2023-01-14 11:35:53 +0600

    [replace] bootstrap styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Home/Home/Home';
