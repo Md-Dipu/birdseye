4a811ac2b537e9b5fbe4a56eac844a16387a2ea2
Md. Dipu <srabun.tangail@gmail.com>
2023-01-16 17:49:56 +0600

    [add] manage plans nav link

--- client/src/Pages/Dashboard/DashboardMenu/DashboardMenu.jsx
+++ client/src/Pages/Dashboard/DashboardMenu/DashboardMenu.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const DashboardMenu = () => {
+    const { user } = useAuth();
+
     return (
         <NavDropdown title="Dashboard" menuVariant="dark" id="basic-nav-dropdown">
             <NavDropdown.Item as={Link} to="/dashboard/bookings">Bookings</NavDropdown.Item>
-            {/* <NavDropdown.Divider /> */}
+            {(user.role === 'admin' || user.role === 'manager') && <>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/dashboard/manage-plans">Manage plans</NavDropdown.Item>
+            </>}
         </NavDropdown>
     );
 };
