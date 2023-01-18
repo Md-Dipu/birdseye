a600b56a441e787f430dfc47d7303dc14a6dddaf
Md. Dipu <srabun.tangail@gmail.com>
2023-01-19 00:16:03 +0600

    [modify] responsive image

--- client/src/Pages/Authentication/Unauthorized/Unauthorized.jsx
+++ client/src/Pages/Authentication/Unauthorized/Unauthorized.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import unauthorizedImg from '../../../assets/images/unauthorized.jpg';
+import './Unauthorized.css';
 
 const Unauthorized = () => {
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '60vh' }}>
             <div className="text-center">
-                <img src={unauthorizedImg} alt="" className="w-50" />
+                <img src={unauthorizedImg} alt="" className="be-unauthorized-image" />
             </div>
         </Container>
     );
