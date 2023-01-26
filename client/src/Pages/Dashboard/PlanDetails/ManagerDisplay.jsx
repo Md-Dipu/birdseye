6bed7380c4f2c78bbb4846f8d9e9ff58f20bb2be
Md. Dipu <srabun.tangail@gmail.com>
2023-01-26 11:51:46 +0600

    [modify] manage information table

--- client/src/Pages/Dashboard/PlanDetails/ManagerDisplay.jsx
+++ client/src/Pages/Dashboard/PlanDetails/ManagerDisplay.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { updateManagerId } from '../../../api/plansAPI';
 import { getUsers } from '../../../api/usersAPI';
 import './ManagerDisplay.css';
@@ -56,22 +56,18 @@ const ManagerDisplay = ({ id, manager, onUpdate }) => {
                 <div className="h5 text-secondary">Manager</div>
                 <Button variant="link" onClick={() => setEdit(true)}>Edit</Button>
             </div>
-            {manager ? <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <table>
-                        <tbody>
-                            {[
-                                ['Name', manager.name],
-                                ['Email', manager.email],
-                                ['Contact', manager.contactNumber]
-                            ].map(item => <tr>
-                                <th className="pe-2">{item[0]}:</th>
-                                <td>{item[1]}</td>
-                            </tr>)}
-                        </tbody>
-                    </table>
-                </Card.Body>
-            </Card> : <div className="text-secondary">Manager not added</div>}
+            {manager ? <table>
+                <tbody>
+                    {[
+                        ['Name', manager.name],
+                        ['Email', manager.email],
+                        ['Contact', manager.contactNumber]
+                    ].map((item, _idx) => <tr key={_idx}>
+                        <th className="pe-3">{item[0]}:</th>
+                        <td>{item[1]}</td>
+                    </tr>)}
+                </tbody>
+            </table> : <div className="text-secondary">Manager not added</div>}
             {edit && <Form className="bg-white shadow position-fixed top-50 start-50 translate-middle border rounded p-3" style={{ width: '18rem', maxWidth: '95%' }} onSubmit={onSubmit}>
                 <Form.Text className="d-block text-center mb-3 fw-bold fs-6">Edit manager</Form.Text>
                 <Form.Group className="mb-3">
