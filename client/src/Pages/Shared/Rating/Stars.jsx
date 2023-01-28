6f5cdaef912f30ed251c8f966b4637e463eb53d1
Md. Dipu <srabun.tangail@gmail.com>
2023-01-28 17:43:54 +0600

    [update] hover effect for selecting stars

--- client/src/Pages/Shared/Rating/Stars.jsx
+++ client/src/Pages/Shared/Rating/Stars.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Stars = ({ rating, setRating }) => {
+    const [hoverStars, setHoverStars] = useState(null);
 
     return (
         <div className="text-center mb-3">
             {[1, 2, 3, 4, 5].map(star => <FontAwesomeIcon
                 key={star}
                 icon={faStar}
-                className={`fs-5 ${(star <= rating) ? 'text-warning' : 'text-secondary'}`}
-                onMouseOver={() => setRating(star)}
+                className={`fs-5 ${(hoverStars ? star <= hoverStars : star <= rating) ? 'text-warning' : 'text-secondary'}`}
+                onClick={() => setRating(star)}
+                onMouseOver={() => setHoverStars(star)}
+                onMouseLeave={() => setHoverStars(null)}
             />)}
         </div>
     );
