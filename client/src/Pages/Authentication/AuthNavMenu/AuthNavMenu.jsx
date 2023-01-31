7f7b73363ea5b179cd707a1638abe04c62fb1447
Md. Dipu <srabun.tangail@gmail.com>
2023-01-31 18:29:29 +0600

    [modify] account link action

--- client/src/Pages/Authentication/AuthNavMenu/AuthNavMenu.jsx
+++ client/src/Pages/Authentication/AuthNavMenu/AuthNavMenu.jsx
@@ -4,13 +4,14 @@ import React from 'react';
 import { Button, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import { scrollToSectionStart } from '../../../utilities/utilities';
 
 const AuthNavMenu = () => {
     const { user, logOut } = useAuth();
 
     return (
         <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> {user.name}</>} menuVariant="dark" id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} to="/dashboard/account">Account</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/settings#account" onClick={scrollToSectionStart('account')}>Account</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item as={Button} variant="link" onClick={logOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
         </NavDropdown>
