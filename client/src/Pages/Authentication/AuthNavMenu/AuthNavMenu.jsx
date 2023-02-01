e50a65bb7af1b6876aac8674aa5380419930c4e5
Md. Dipu <srabun.tangail@gmail.com>
2023-02-01 18:38:53 +0600

    [add] settings nav link

--- client/src/Pages/Authentication/AuthNavMenu/AuthNavMenu.jsx
+++ client/src/Pages/Authentication/AuthNavMenu/AuthNavMenu.jsx
@@ -1,6 +1,6 @@
-import { faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Button, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
@@ -12,6 +12,7 @@ const AuthNavMenu = () => {
     return (
         <NavDropdown title={<><FontAwesomeIcon icon={faUser} /> {user.name}</>} menuVariant="dark" id="basic-nav-dropdown">
             <NavDropdown.Item as={Link} to="/settings#account" onClick={scrollToSectionStart('account')}>Account</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/settings">Settings</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item as={Button} variant="link" onClick={logOut}><FontAwesomeIcon icon={faSignOutAlt} /> Log out</NavDropdown.Item>
         </NavDropdown>
