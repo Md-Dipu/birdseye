e738c276d1779f92af5d2743bcd4fb2af99dd2fa
Md. Dipu <srabun.tangail@gmail.com>
2023-02-02 19:34:03 +0600

    [add] Bearer token

--- client/src/utilities/API.js
+++ client/src/utilities/API.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+const axiosConfig = () => {
+    const headers = {};
+    const idToken = localStorage.getItem('idToken');
+    if (idToken) {
+        headers.authorization = `Bearer ${localStorage.getItem('idToken')}`;
+    }
+
+    return { headers };
+};
+
 class API {
     constructor(route) {
         this.serverRouteURL = process.env.REACT_APP_SERVER_BACKEND_API_URL + route;
@@ -8,25 +18,25 @@ class API {
     /** get api */
     get(extension) {
         const apiURL = this.serverRouteURL + extension;
-        return axios.get(apiURL);
+        return axios.get(apiURL, axiosConfig());
     }
 
     /** post api */
     post(data) {
         const apiURL = this.serverRouteURL;
-        return axios.post(apiURL, data);
+        return axios.post(apiURL, data, axiosConfig());
     }
 
     /** patch api */
     patch(extension, data) {
         const apiURL = this.serverRouteURL + extension;
-        return axios.patch(apiURL, data);
+        return axios.patch(apiURL, data, axiosConfig());
     }
 
     /** delete api */
     delete(extension) {
         const apiURL = this.serverRouteURL + extension;
-        return axios.delete(apiURL);
+        return axios.delete(apiURL, axiosConfig());
     }
 }
 
