76f4130061a34ce38d0fb783424a37b22b240767
Md. Dipu <srabun.tangail@gmail.com>
2023-02-03 20:52:47 +0600

    [remove] auth verification from get/user route

--- server/routes/v1/users.route.js
+++ server/routes/v1/users.route.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const usersController = require("../../controllers/users.controller");
-const authorization = require("../../middleware/authorization");
 const verifyToken = require("../../middleware/verifyToken");
 
 const router = express.Router();
 
 router.route("/")
     .post(usersController.createNewUserController)
-    .get(verifyToken, authorization("admin", "manager"), usersController.getUsersController);
+    .get(usersController.getUsersController);
 
 router.route("/id/:id")
     .patch(verifyToken, usersController.updateUserByIdController)
