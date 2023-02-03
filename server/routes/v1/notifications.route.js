1107a7d2316324d4f9b685d7e51b8a08b8e5beba
Md. Dipu <srabun.tangail@gmail.com>
2023-02-03 18:16:52 +0600

    [update] verify user

--- server/routes/v1/notifications.route.js
+++ server/routes/v1/notifications.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const notificationControllers = require("../../controllers/notifications.controller");
+const verifyToken = require("../../middleware/verifyToken");
 
 const router = express.Router();
+
+router.use(verifyToken);
 
 router.route("/").post(notificationControllers.createNotificationController);
 
