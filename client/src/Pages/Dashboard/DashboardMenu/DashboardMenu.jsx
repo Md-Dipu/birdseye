f2ebce53de338c44a0ebebb6db635d5dbe10fa59
Md. Dipu <srabun.tangail@gmail.com>
2023-02-05 21:31:59 +0600

    [add] notification nav link

--- client/src/Pages/Dashboard/DashboardMenu/DashboardMenu.jsx
+++ client/src/Pages/Dashboard/DashboardMenu/DashboardMenu.jsx
@@ -9,6 +9,7 @@ const DashboardMenu = () => {
     return (
         <NavDropdown title="Dashboard" menuVariant="dark" id="basic-nav-dropdown">
             <NavDropdown.Item as={Link} to="/dashboard/bookings">Bookings</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/dashboard/notifications">Notifications</NavDropdown.Item>
             {(user.role === 'admin' || user.role === 'manager') && <>
                 <NavDropdown.Divider />
                 <NavDropdown.Item as={Link} to="/dashboard/manage-plans">Manage plans</NavDropdown.Item>
