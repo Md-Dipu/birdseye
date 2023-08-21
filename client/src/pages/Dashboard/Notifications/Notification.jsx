52b32f79009f5616f112a35c9cbeb26a6f57670d
Md. Dipu <srdipu@hotmail.com>
2023-08-21 14:37:12 +0600

    [update] display notifications list

---
+++ client/src/pages/Dashboard/Notifications/Notification.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Badge, Card } from 'react-bootstrap';
+import useAuth from '../../../hooks/useAuth';
+
+const Notification = props => {
+    const { user } = useAuth();
+    const isSeen = Boolean(props.seenBy.find(id => id === user._id));
+
+    return (
+        <Card className="my-1 p-3 rounded-0" style={{ borderLeft: `2px var(${isSeen ? '--bs-success' : '--bs-danger'}) solid` }}>
+            <Card.Subtitle className="fw-semibold text-secondary">{props.title} {!isSeen && <Badge pill bg="warning">New</Badge>}</Card.Subtitle>
+            <Card.Text className={`${isSeen ? '' : 'fw-semibold'} text-secondary text-truncate`}>{props.message}</Card.Text>
+        </Card>
+    );
+};
+
+export default Notification;
--- client/src/pages/Dashboard/Notifications/index.jsx
+++ client/src/pages/Dashboard/Notifications/index.jsx
@@ -1,91 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { getNotificationById, getNotifications } from '../../../api/notificationsAPI';
-import { updateUserById } from '../../../api/usersAPI';
-import useAuth from '../../../hooks/useAuth';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
+import { getNotifications } from '../../../api/notificationsAPI';
+import Loading from '../../Shared/Loading';
+import Notification from './Notification';
 
 const Notifications = () => {
-    const [notifications, setNotifications] = useState([]);
-    const [showNotification, setShowNotifications] = useState(null);
-    const [data, setData] = useState(null);
-
-    const { user } = useAuth();
+    const [count, setCount] = useState(0);
+    const [notifications, setNotifications] = useState(null);
 
     useEffect(() => {
         getNotifications()
-            .then(res => setNotifications(res.data.data))
-            .catch(error => console.warn(error.message));
+            .then(({ data: res }) => {
+                setCount(res.count);
+                setNotifications(res.data.reverse());
+            });
     }, []);
 
-    // stating time id setter
-    useEffect(() => {
-        if (!showNotification && notifications.length) {
-            setShowNotifications(notifications[0]._id);
-        }
-    }, [notifications, showNotification]);
-
-    // getting notification data
-    useEffect(() => {
-        if (showNotification) {
-            getNotificationById(showNotification)
-                .then(res => setData(res.data.data))
-                .catch(error => console.warn(error.message));
-        }
-    }, [showNotification]);
-
-    const handleRoleRequest = (event, userId, role) => {
-        updateUserById(userId, { role }).then(() => event.target.disabled = true)
-            .catch(error => console.warn(error.message));
-    };
+    if (!notifications) {
+        return (
+            <>
+                <Helmet title="Notifications" />
+                <Loading height="70" />
+            </>
+        );
+    }
 
     return (
-        <Container className="my-3">
+        <Container className="my-3" style={{ minHeight: "60vh" }}>
             <Helmet title="Notifications" />
-            <Row>
-                <Col md="3">
-                    <div className="h6 fw-bold">Notifications</div>
-                    <div>
-                        {notifications.map((notification, index) => {
-                            const read = notification.seenBy.find(id => id === user._id);
-
-                            const onClick = () => {
-                                notifications[index].seenBy.push(user._id);
-                                setShowNotifications(notification._id);
-                                setNotifications(notifications);
-                            };
-
-                            return (
-                                <div key={notification._id} className={`border rounded my-2 p-2 ${read ? 'text-secondary' : 'border-dark'}`} onClick={onClick} style={{ cursor: 'pointer' }}>
-                                    {notification.title && <div className="h6">{notification.title}</div>}
-                                    {notification.message && <div className="text-nowrap" style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>{notification.message}</div>}
-                                </div>
-                            );
-                        })}
-                    </div>
-                </Col>
-                <Col id="notification-area">
-                    {data ? <div className="text-secondary">
-                        <div className="fw-bold">{data.title || '<no subject added>'}</div>
-                        <div className="mb-3">{data.message}</div>
-                        {(data.type === 'role-request') && <div>
-                            <div className="fw-bold">Request</div>
-                            <table className="mb-3">
-                                <tbody>
-                                    {[
-                                        ['User id', data.requestData.userId],
-                                        ['Role', data.requestData.role],
-                                    ].map((item, idx) => <tr key={idx}>
-                                        <th className="text-nowrap pe-3" style={{ verticalAlign: 'baseline' }}>{item[0]}:</th>
-                                        <td>{item[1]}</td>
-                                    </tr>)}
-                                </tbody>
-                            </table>
-                            <Button variant="success" size="sm" className="rounded-0" onClick={e => handleRoleRequest(e, data.requestData.userId, data.requestData.role)}>Approve</Button>
-                        </div>}
-                    </div> : 'Data not found'}
-                </Col>
-            </Row>
+            <div className="h3 text-secondary">Notifications</div>
+            {count ? (
+                <Row xs={1}>
+                    {notifications.map(notification => <Col>
+                        <Notification {...notification} />
+                    </Col>)}
+                </Row>
+            ) : <div className="text-secondary">No notifications found</div>}
         </Container>
     );
 };
